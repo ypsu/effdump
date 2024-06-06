206b67b6af9fb29a2fee1fb749e7b8c57e407737
Balazs Kezes <3j4b0y1i@anonaddy.me>
2024-06-06 08:41:54 +0200

    marshal: add a test for limits

--- internal/marshal/marshal_test.go
+++ internal/marshal/marshal_test.go
@@ -9,8 +9,20 @@ import (
 
 func TestHeader(t *testing.T) {
 	es := marshal.Entries{{"somekey", "somevalue"}}
-	data, _ := es.MarshalBinary()
+	data, err := es.MarshalBinary()
+	if err != nil {
+		t.Errorf("MarshalBinary() = %v, want no error.", err)
+	}
 	if !strings.HasPrefix(string(data), "effdump0") {
-		t.Error("effdump0 header missing.")
+		t.Errorf(`MarshalBinary() = %q, want "effdump0".`, string(data)[:8])
+	}
+}
+
+func TestLimit(t *testing.T) {
+	es := marshal.Entries{{}}
+	es[0].Key = strings.Repeat("x", 1<<16)
+	_, err := es.MarshalBinary()
+	if err == nil {
+		t.Errorf("MarshalBinary() = nil, want error.")
 	}
 }
