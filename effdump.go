d4eb3ec380117c7798852caa49ca772d6cfaf08f
Balazs Kezes <3j4b0y1i@anonaddy.me>
2024-06-08 08:02:30 +0200

    effect: refactor marshal and main functionality into more appropriate packages

--- effdump.go
+++ effdump.go
@@ -2,29 +2,18 @@
 package effdump
 
 import (
-	"cmp"
+	"flag"
 	"fmt"
+	"os"
 	"slices"
 
-	"github.com/ypsu/effdump/internal/marshal"
+	"github.com/ypsu/effdump/internal/edmain"
+	"github.com/ypsu/effdump/internal/effect"
 )
-
-// Overrideable for testing purposes.
-var run func(name string, es marshal.Entries) = effdumpmain
-
-func stringify(v any) string {
-	if s, ok := v.(fmt.Stringer); ok {
-		return s.String()
-	}
-	if bs, ok := v.([]byte); ok {
-		return string(bs)
-	}
-	return fmt.Sprint(v)
-}
 
 // Dump represesents an effdump.
 type Dump struct {
-	es marshal.Entries
+	effects []effect.Effect
 }
 
 // New initializes a new Dump.
@@ -34,34 +23,32 @@ func New() *Dump {
 
 // Add adds a key value into the dump.
 func (d *Dump) Add(key, value any) {
-	d.es = append(d.es, marshal.Entry{stringify(key), stringify(value)})
+	d.effects = append(d.effects, effect.Effect{effect.Stringify(key), effect.Stringify(value)})
 }
 
 // AddMap adds each entry of the map to the dump.
 // It's a standalone method due to a Go limitation around generics.
 func AddMap[M ~map[K]V, K comparable, V any](d *Dump, m M) {
-	d.es = slices.Grow(d.es, len(m))
+	d.effects = slices.Grow(d.effects, len(m))
 	for k, v := range m {
 		d.Add(k, v)
 	}
 }
 
 // Run writes/diffs the effdump named `name`.
-// This is meant to be overtake the main() function once the effect map is computed.
+// This is meant to be overtake the main() function once the effect map is computed, this function never returns.
 // Its behavior is dependent on the command line, see the package comment.
 func (d *Dump) Run(name string) {
-	slices.SortFunc(d.es, func(a, b marshal.Entry) int { return cmp.Compare(a.Key, b.Key) })
-	run(name, d.es)
-}
-
-// TODO: Create this one too:
-// RunCustomized(name string, stdout, stderr io.Writer, flagset *flag.FlagSet, env []string) error
-// Run() then just does this:
-// if err := RunCustomized(...); err != nil { ... }
-// os.Exit(0)
-
-func effdumpmain(name string, es marshal.Entries) {
-	for _, e := range es {
-		fmt.Printf("%s/%s: %q\n", name, e.Key, e.Value)
+	err := (&edmain.Params{
+		Name:    name,
+		Effects: d.effects,
+		Stdout:  os.Stdout,
+		Flagset: flag.CommandLine,
+		Env:     os.Environ(),
+	}).Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
+	os.Exit(0)
 }
---
+++ internal/edmain/edmain.go
@@ -0,0 +1,30 @@
+// Package edmain (EffDump MAIN) implements the CLI integration of the tool.
+package edmain
+
+import (
+	"cmp"
+	"flag"
+	"fmt"
+	"io"
+	"slices"
+
+	"github.com/ypsu/effdump/internal/effect"
+)
+
+// Params contains most of the I/O dependencies for the Run().
+type Params struct {
+	Name    string
+	Effects []effect.Effect
+	Stdout  io.Writer
+	Flagset *flag.FlagSet
+	Env     []string
+}
+
+// Run runs effdump's main CLI logic.
+func (p *Params) Run() error {
+	slices.SortFunc(p.Effects, func(a, b effect.Effect) int { return cmp.Compare(a.Key, b.Key) })
+	for _, e := range p.Effects {
+		fmt.Fprintf(p.Stdout, "%s/%s: %q\n", p.Name, e.Key, e.Value)
+	}
+	return nil
+}
