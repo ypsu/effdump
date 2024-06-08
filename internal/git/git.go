412d1bdee8ba5634ef5ee0018165d480fb28b7d7
Balazs Kezes <3j4b0y1i@anonaddy.me>
2024-06-08 15:00:11 +0200

    git: implement version fetching

--- internal/git/git.go
+++ internal/git/git.go
@@ -1,23 +1,43 @@
 // Package git implements the git interaction for effdump.
 package git
 
-import "context"
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+)
 
 // VersionSystem implements git version lookup via parsing `git` CLI tool's output.
-// It uses the first 8 characters of the full commit ID for the version strings.
+// It uses the first ~7 characters of the full commit ID for the version strings.
 type VersionSystem struct{}
 
 // New returns a new git VersionSystem.
 func New() *VersionSystem { return &VersionSystem{} }
 
 // Fetch returns the HEAD commit and whether working dir is clean or not.
-func (*VersionSystem) Fetch(context.Context) (version string, clean bool, err error) {
-	// TODO: implement
-	return "1234abcd", true, nil
+func (vs *VersionSystem) Fetch(ctx context.Context) (version string, clean bool, err error) {
+	head, err := vs.Resolve(ctx, "HEAD")
+	if err != nil {
+		return "", false, fmt.Errorf("git fetch HEAD: %v", err)
+	}
+	clean = true
+	_, err = exec.Command("git", "diff-index", "--quiet", "HEAD").Output()
+	if err != nil {
+		clean = false
+	}
+	return head, clean, nil
 }
 
 // Resolve resolves references such as HEAD or "ab12" to the full version string.
-func (*VersionSystem) Resolve(context.Context, string) (version string, err error) {
-	// TODO: implement
-	return "1234abcd", nil
+func (*VersionSystem) Resolve(_ context.Context, rev string) (version string, err error) {
+	output, err := exec.Command("git", "rev-parse", "--short", rev).Output()
+	if err != nil {
+		return "", fmt.Errorf("git exec rev-parse: %v", err)
+	}
+	fields := bytes.Fields(output)
+	if len(fields) != 1 {
+		return "", fmt.Errorf("git split rev-parse: expected one field, got %q", output)
+	}
+	return string(bytes.TrimSpace(fields[0])), nil
 }
