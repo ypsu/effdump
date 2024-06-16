7e395f5e4de569b5f3648c21faab303fdc368751
Balazs Kezes <3j4b0y1i@anonaddy.me>
2024-06-16 11:28:26 +0200

    fmtdiff: collapse common ranges at the beginning and at the end
    and add a testcase that demos this.

--- internal/fmtdiff/unified.go
+++ internal/fmtdiff/unified.go
@@ -12,7 +12,12 @@ import (
 func Unified(d andiff.Diff) string {
 	w := &strings.Builder{}
 	w.Grow(256)
-	for i := 0; i < d.Ops[0]; i++ {
+	start := 0
+	if d.Ops[0] > 5 {
+		fmt.Fprintf(w, "@@ %d common lines @@\n", d.Ops[0]-3)
+		start = d.Ops[0] - 3
+	}
+	for i := start; i < d.Ops[0]; i++ {
 		fmt.Fprintf(w, " %s\n", d.LT[i])
 	}
 	x, y := d.LT[d.Ops[0]:], d.RT[d.Ops[0]:]
@@ -23,8 +28,15 @@ func Unified(d andiff.Diff) string {
 		for j := 0; j < d.Ops[i+1]; j++ {
 			fmt.Fprintf(w, "+%s\n", y[j])
 		}
-		for j := 0; j < d.Ops[i+2]; j++ {
-			fmt.Fprintf(w, " %s\n", x[d.Ops[i]+j])
+		if d.Ops[i+2] > 5 && i+3 == len(d.Ops) {
+			for j := 0; j < 3; j++ {
+				fmt.Fprintf(w, " %s\n", x[d.Ops[i]+j])
+			}
+			fmt.Fprintf(w, "@@ %d common lines @@\n", d.Ops[i+2]-3)
+		} else {
+			for j := 0; j < d.Ops[i+2]; j++ {
+				fmt.Fprintf(w, " %s\n", x[d.Ops[i]+j])
+			}
 		}
 		x, y = x[d.Ops[i]+d.Ops[i+2]:], y[d.Ops[i+1]+d.Ops[i+2]:]
 	}
