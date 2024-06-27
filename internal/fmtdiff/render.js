7631982a60c1e605a32af8b4c55ec5269b7d2fce
Balazs Kezes <3j4b0y1i@anonaddy.me>
2024-06-27 06:24:08 +0200

    fmtdiff: format html output into two columns

--- internal/fmtdiff/render.js
+++ internal/fmtdiff/render.js
@@ -1,18 +1,32 @@
-// Note about variable conventions:
-//
-// - h_ prefixed vars are UI elements defined in header.html.
-// - d_ prefixed vars come from the generated data section.
-// - everything else is standard library or from this file.
+// Note about variable conventions: h_ prefixed vars are UI elements defined in header.html.
 
 function main() {
   let h = ''
   for (let name in diffs) {
     h += `<p>${name}</p>`
-    h += `<pre>`
-    for (let id of diffs[name].rt) {
-      h += `${lines[id]}\n`
+    let hlt = ''
+    let hrt = ''
+    let x = diffs[name].lt
+    let y = diffs[name].rt
+    let xi = 0
+    let yi = 0
+    for (let i = 0; i < diffs[name].ops.length; i += 3) {
+      let del = diffs[name].ops[i]
+      let add = diffs[name].ops[i + 1]
+      let keep = diffs[name].ops[i + 2]
+      for (let i = 0; i < del; i++) hlt += lines[x[xi++]] + '\n'
+      for (let i = del; i < add; i++) hlt += '\n'
+      for (let i = 0; i < add; i++) hrt += lines[y[yi++]] + '\n'
+      for (let i = add; i < del; i++) hrt += '\n'
+      while (keep-- > 0) {
+        hlt += lines[x[xi++]] + '\n'
+        hrt += lines[y[yi++]] + '\n'
+      }
     }
-    h += `</pre>`
+    h += `<table>\n`
+    h += `  <td><pre>${hlt}</pre>`
+    h += `  <td><pre>${hrt}</pre>`
+    h += `</table>`
     h += `<hr>`
   }
 
