e583255cb40e6984c41ba0dc828babbc85b4c05d
Balazs Kezes <3j4b0y1i@anonaddy.me>
2024-08-18 07:35:32 +0200

    docs: add documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# effdump
+
+effdump is a Go module to generate and diff textual effects across git commits.
+See https://pkg.go.dev/github.com/ypsu/effdump for the documentation.
+
+Note: this project uses https://ypsu.github.io/featver as its versioning scheme.
+The project will enter the more stable v1 version only when it has users already.
--- effdump.go
+++ effdump.go
@@ -1,4 +1,12 @@
 // Package effdump implements the CLI tool for working with effdumps.
+//
+// effdump testing is like output or golden testing but the outputs don't have to be committed into the git repository.
+// It can render the diffs both in unified and HTML format and deduplicates the individual diffs.
+// Convenient CLI usage is the module's main design goal.
+//
+// To use it create a new dump with [New], [Add] a bunch of key/value pairs, and call [Run].
+// effdump takes over the rest, it makes the package into a CLI tool.
+// See https://github.com/ypsu/effdump/tree/main/example-markdown/README.md and https://github.com/ypsu/effdump/tree/main/example-deployment/README.md for how exactly it works.
 package effdump
 
 import (
@@ -33,11 +41,14 @@ func New(name string) *Dump {
 }
 
 // Add adds a key value into the dump.
+// The key and value are automatically stringified.
+// It stringifies structs and lists without a String() function into json.
 func (d *Dump) Add(key, value any) {
 	d.params.Effects = append(d.params.Effects, keyvalue.KV{edmain.Stringify(key), edmain.Stringify(value)})
 }
 
 // AddMap adds each entry of the map to the dump.
+// The keys and values are stringified the same way as in [Add].
 // It's a standalone method due to a Go limitation around generics.
 func AddMap[M ~map[K]V, K comparable, V any](d *Dump, m M) {
 	d.params.Effects = slices.Grow(d.params.Effects, len(m))
@@ -66,9 +77,8 @@ func (d *Dump) RegisterFlags(fs *flag.FlagSet) {
 	d.params.RegisterFlags(fs)
 }
 
-// Run writes/diffs the effdump named `name`.
-// This is meant to be overtake the main() function once the effect map is computed, this function never returns.
-// Its behavior is dependent on the command line, see the package comment.
+// Run implements the CLI interface.
+// This is meant to overtake the main() function: this function never returns.
 func (d *Dump) Run(ctx context.Context) {
 	if !d.registeredFlags {
 		d.RegisterFlags(flag.CommandLine)
